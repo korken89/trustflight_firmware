3f7855a15f60dd3162e54a3792b98b99706398f4
Emil Fresk <emil.fresk@gmail.com>
2018-08-17 16:59:56 +0200

    Removed unnecessary code

--- trustflight/src/main.rs
+++ trustflight/src/main.rs
@@ -6,27 +6,7 @@ extern crate panic_halt;
 extern crate trustflight_hal;
 
 use trustflight_hal::cortex_m::asm;
-use trustflight_hal::cortex_m_rt::ExceptionFrame;
 
-entry!(main);
-
-#[inline(never)]
-fn main() -> ! {
-    loop {
-        asm::bkpt();
-    }
-}
-
-// define the hard fault handler
-exception!(HardFault, hard_fault);
-
-fn hard_fault(_ef: &ExceptionFrame) -> ! {
-    panic!("hard fault");
-}
-
-// define the default exception handler
-exception!(*, default_handler);
-
-fn default_handler(_irqn: i16) {
-    panic!("default handler");
-}
+entry!(|| loop {
+    asm::bkpt();
+});
