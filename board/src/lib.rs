ff0e62dfb187ec9128f0bb8ad288b2a4d08a509c
Emil Fresk <emil.fresk@gmail.com>
2018-08-25 17:53:07 +0200

    Added testing skeleton in board

--- board/src/lib.rs
+++ board/src/lib.rs
@@ -6,3 +6,13 @@ extern crate stm32f3;
 extern crate trustflight_board_trait;
 
 pub use cortex_m_rt::*;
+
+//
+// Testing requirements
+//
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
+#[cfg(test)]
+mod tests;
