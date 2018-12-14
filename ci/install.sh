28c289ad0f75e6c5c748f6bda032be9699fdb99d
Emil Fresk <emil.fresk@gmail.com>
2018-12-14 14:24:15 +0100

    Fixed mdcheckr

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 main() {
     if [ $TARGET = docs ]; then
         mkdir mdcheck
-        curl https://raw.githubusercontent.com/korken89/mdcheckr/master/mdcheckr -o mdcheck/mdcheckr
+        curl https://raw.githubusercontent.com/mike42/mdcheckr/master/mdcheckr -o mdcheck/mdcheckr
         chmod +x mdcheck/mdcheckr
     else
         rustup target add thumbv7em-none-eabihf
